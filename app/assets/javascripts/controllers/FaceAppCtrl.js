ca481ea9f1af2fee48604f6a8fd33d15b0b7b5a7
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-03-19 11:18:41 -0600

    feat: (Angular) angular-facebook addon

--- app/assets/javascripts/controllers/FaceAppCtrl.js
+++ app/assets/javascripts/controllers/FaceAppCtrl.js
@@ -1,67 +1,45 @@
 (function(){
 	'use strict';
 
-	angular.module('odiApp', ['ngRoute'])
+	angular.module('odiApp', ['ngRoute', 'facebook'])
+		.config(function(FacebookProvider){
+			FacebookProvider.init('1560451627557497');
+		})
 		.controller('FaceAppCtrl', 
-			function FaceAppCtrl () {
-				this.showFBbutton = true;
+			function FaceAppCtrl ($scope, Facebook) {
+				$scope.login = function(){
+					Facebook.login(function(response){
+						console.log(response);
+					});
+				};
 
-					// This is called with the results from from FB.getLoginStatus().
-				  function statusChangeCallback(response) {
-				    console.log('statusChangeCallback');
-				    console.log(response);
-				    // The response object is returned with a status field that lets the
-				    // app know the current login status of the person.
-				    // Full docs on the response object can be found in the documentation
-				    // for FB.getLoginStatus().
-				    if (response.status === 'connected') {
-				      // Logged into your app and Facebook.
-				      testAPI();
-				    } else if (response.status === 'not_authorized') {
-				      // The person is logged into Facebook, but not your app.
-				      document.getElementById('status').innerHTML = 'Please log ' +
-				        'into this app.';
-				    } else {
-				      // The person is not logged into Facebook, so we're not sure if
-				      // they are logged into this app or not.
-				      document.getElementById('status').innerHTML = 'Please log ' +
-				        'into Facebook.';
-				    }
-				  }
+				$scope.getLoginStatus = function() {
+			      Facebook.getLoginStatus(function(response) {
+			        if(response.status === 'connected') {
+			          $scope.loggedIn = true;
+			        } else {
+			          $scope.loggedIn = false;
+			        }
+			      });
+			    };
 
-				  function testAPI() {
-				    console.log('Welcome!  Fetching your information.... ');
-				    FB.api('/me', function(response) {
-				      console.log('Id:'+response.id);
-				      console.log('Name:' + response.name);
-				      console.log('First Name:'+response.first_name);
-				      console.log('Last Name:'+response.last_name);
-				      console.log('Locale:'+response.locale);
-				      
-				      document.getElementById('status').innerHTML =
-				        'Thanks for logging in, ' + response.name + '!';
-				    });
-				  }
+			    $scope.getLoginStatus = function() {
+			      Facebook.getLoginStatus(function(response) {
+			        if(response.status === 'connected') {
+			          $scope.loggedIn = true;
+			        } else {
+			          $scope.loggedIn = false;
+			        }
+			      });
+			    };
 
-				  // This function is called when someone finishes with the Login
-				  // Button.  See the onlogin handler attached to it in the sample
-				  // code below.
-				
+			    $scope.$watch(function() {
+				  // This is for convenience, to notify if Facebook is loaded and ready to go.
+				  return Facebook.isReady();
+				}, function(newVal) {
+				  // You might want to use this to disable/show/hide buttons and else
+				  $scope.facebookReady = true;
+				});
 
-				window.fbAsyncInit = function() {
-				    FB.init({
-				      appId      : '1560451627557497',
-				      xfbml      : true,
-				      version    : 'v2.2'
-				    });
-				  };
-
-				  (function(d, s, id){
-				     var js, fjs = d.getElementsByTagName(s)[0];
-				     if (d.getElementById(id)) {return;}
-				     js = d.createElement(s); js.id = id;
-				     js.src = "//connect.facebook.net/en_US/sdk.js";
-				     fjs.parentNode.insertBefore(js, fjs);
-				   }(document, 'script', 'facebook-jssdk'));
 			});
 })();
