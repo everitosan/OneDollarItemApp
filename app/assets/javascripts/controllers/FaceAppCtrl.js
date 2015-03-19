abcf244dec4e7f574cf2f364591166a9fc7f8fb9
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-03-19 10:32:22 -0600

    fix: (Facebook) login and angular integration

--- app/assets/javascripts/controllers/FaceAppCtrl.js
+++ app/assets/javascripts/controllers/FaceAppCtrl.js
@@ -46,11 +46,7 @@
 				  // This function is called when someone finishes with the Login
 				  // Button.  See the onlogin handler attached to it in the sample
 				  // code below.
-				this.checkLoginState = function() {
-				    FB.getLoginStatus(function(response) {
-				      statusChangeCallback(response);
-				    });
-				  }
+				
 
 				window.fbAsyncInit = function() {
 				    FB.init({
