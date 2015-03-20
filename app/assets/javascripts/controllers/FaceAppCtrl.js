946c3c80a16d22998ba27ecb5a149afcc5796e63
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-03-19 18:25:45 -0600

    feat: (Facebook) complete connection and use of profile picture

--- app/assets/javascripts/controllers/FaceAppCtrl.js
+++ app/assets/javascripts/controllers/FaceAppCtrl.js
@@ -2,47 +2,63 @@
 	'use strict';
 
 	angular.module('odiApp', ['ngRoute', 'facebook'])
-		.config(function(FacebookProvider){
-			FacebookProvider.init('1560451627557497');
-		})
-		.controller('FaceAppCtrl', 
-			function FaceAppCtrl ($scope, Facebook) {
+		.config(function(FacebookProvider) {
+	     // Set your appId through the setAppId method or
+	     // use the shortcut in the initialize method directly.
+	     FacebookProvider.init('1560451627557497');
+	  })
+		.controller('authenticationCtrl', function($scope, Facebook) {
+			$scope.user = {};
+			$scope.loggedIn = false;
 
-				$scope.facebookReady = false;
-				
-				$scope.login = function(){
-					Facebook.login(function(response){
-						console.log(response);
-					});
-				};
+	    $scope.IntentLog = function () {
+	    	$scope.login();
+	    };
 
-				$scope.getLoginStatus = function() {
-			      Facebook.getLoginStatus(function(response) {
-			        if(response.status === 'connected') {
-			          $scope.loggedIn = true;
-			        } else {
-			          $scope.loggedIn = false;
-			        }
-			      });
-			    };
+			$scope.login = function() { 
+		  	Facebook.login(function(response) {
+		  		
+		  		if (response.status === 'connected'){
+		  			$scope.loggedIn = true;
+		  			$scope.me();
+		  		}
+		  		else if (response.status === 'not_authorized') {
+	        	$scope.loggedIn = false;
+	        	$scope.auth = false;
+	        }
+	        else {
+	        	$scope.loggedIn = false;
+	        }
+		  	}, {scope:'public_profile, email'});
+			};
 
-			    $scope.getLoginStatus = function() {
-			      Facebook.getLoginStatus(function(response) {
-			        if(response.status === 'connected') {
-			          $scope.loggedIn = true;
-			        } else {
-			          $scope.loggedIn = false;
-			        }
-			      });
-			    };
+	    $scope.me = function() {
+	      Facebook.api('/me', function(response) {
+	        $scope.user = response;
+	        setDatatoPostgresql();
+	        setPhoto('http://graph.facebook.com/'+response.id+'/picture?type=small');
+	      });
+	    };
 
-			    $scope.$watch(function() {
-				  // This is for convenience, to notify if Facebook is loaded and ready to go.
-				  return Facebook.isReady();
-				}, function(newVal) {
-				  // You might want to use this to disable/show/hide buttons and else
-				  $scope.facebookReady = true;
-				});
+	    function setDatatoPostgresql() {
+	    	console.log($scope.user);
+	    }
 
-			});
+	    function setPhoto (url){
+	    	var photoElement = document.getElementById('photoUrl');
+	    	photoElement.src = url;
+	    }
+
+      $scope.getLoginStatus = function() {
+		    Facebook.getLoginStatus(function(response) {
+		      if(response.status === 'connected') {
+		        $scope.loggedIn = true;
+		      }
+		      else {
+		        $scope.loggedIn = false;
+		      }
+		    });
+		  };
+
+	});
 })();
