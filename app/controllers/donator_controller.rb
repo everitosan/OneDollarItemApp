1e1b0034385ef293982c1c84db99734721029fda
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-03-20 22:10:47 -0600

    fix: (Backend) Crates user if new at db

--- app/controllers/donator_controller.rb
+++ app/controllers/donator_controller.rb
@@ -17,9 +17,11 @@ class DonatorController < ApplicationController
   		@NewUser.fb_id = params[:id]
   		@NewUser.name = params[:name]
   		@NewUser.email = params[:email]
+  		@NewUser.first_n = params[:first_n]
+  		@NewUser.last_n = params[:last_n]
+  		@NewUser.locate = params[:locate]
   		@NewUser.save
   		@User = User.last
-  		#render json: { status: :ok, owner: @NewUser}
   	end
   	@idItem = params[:item_id]
   	
