9b3b1c3730e5e2509867e10c4c5598a86fb9badf
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-03-26 11:27:10 -0600

    feat: (Paypal) introduce the rails management for url checkout

--- app/controllers/odi_controller.rb
+++ app/controllers/odi_controller.rb
@@ -11,6 +11,10 @@ class OdiController < ApplicationController
   def results
   end
 
+	def suma(num1, num2) 
+		return num1 + num2
+	end
+
   def set_csrf_cookie_for_ng
 		cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
 	end
@@ -18,6 +22,6 @@ class OdiController < ApplicationController
 	protected
 		def verified_request?
 	    super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
-		end
+	end
 
 end
--- app/helpers/odi_helper.rb
+++ app/helpers/odi_helper.rb
@@ -1,2 +1,24 @@
 module OdiHelper
+
+	def paypal_url(id_item, des_item) 
+	values = {
+      :business => 'eve-facilitator@bye.com',
+      :cmd => '_donations',
+      :upload => 1,
+      :return => 'http://localhost:3000/',
+      :amount => '1.3',
+      :item_name => des_item
+    }
+=begin
+    line_items.each_with_index do |item, index|
+      values.merge!({
+        "amount_#{index+1}" => item.unit_price,
+        "item_name_#{index+1}" => item.product.name,
+        "item_number_#{index+1}" => item.id,
+        "quantity_#{index+1}" => item.quantity
+      })
+    end
+=end    
+    "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
+	end
 end
