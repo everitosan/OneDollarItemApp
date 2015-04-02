9e2c1ab3a24f31ed369a61839c9ca155d7377817
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-04-01 20:32:25 -0600

    fix: (RailsAngular) templates for directives cool

--- app/assets/javascripts/odi.js
+++ app/assets/javascripts/odi.js
@@ -4,6 +4,6 @@
 (function(){
 	'use strict';
 
-	angular.module('odiApp', ['ngRoute','odiApp.controllers', 'odiApp.services', 'odiApp.directives']);
+	angular.module('odiApp', ['ngRoute', 'odiApp.controllers', 'odiApp.services', 'odiApp.directives']);
 
 })();
