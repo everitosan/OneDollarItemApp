b0e117da9e74930811df59728f69139448d2f01e
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-04-13 16:06:19 -0500

    fix: (mailBackend) render removal

--- app/controllers/mailer_controller.rb
+++ app/controllers/mailer_controller.rb
@@ -2,6 +2,7 @@ class MailerController < ApplicationController
 
 	def contact
 		ContactEmail.message_email(params[:name], params[:email], params[:message]).deliver_now
+		render :nothing => true
 	end
 
 end
