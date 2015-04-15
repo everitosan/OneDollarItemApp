83757f7bda9aa8fa6ece65c53906d6923821bb14
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-04-15 03:52:04 -0500

    fix: (log) paypal ipn

--- app/controllers/ipn_controller.rb
+++ app/controllers/ipn_controller.rb
@@ -2,7 +2,7 @@ class IpnController < ApplicationController
 	protect_from_forgery :excet => [:newOwner]
 
 	def newOwner
-
+		logger.debug "/*********************** IPN Notification ********/"
 		setOwner(params[:payment_status], params[:payer_email], params[:item_name])
 		
 
@@ -11,6 +11,7 @@ class IpnController < ApplicationController
 
 	def setOwner (status, owner, item)
 		if status == "Completed"
+			logger.debug "/*********************** IPN Notification COMPLETED ********/"
 			@currUser = User.find_by(emailPayment: owner)
 		  	@currentItem = Item.find_by_description(item)
 		  	
