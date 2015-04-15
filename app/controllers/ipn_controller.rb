d74e8a97547e9ca2346915940ad179872d1a6630
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-04-15 10:42:59 -0500

    fix: (paypal) ion check for secret

--- app/controllers/ipn_controller.rb
+++ app/controllers/ipn_controller.rb
@@ -3,14 +3,14 @@ class IpnController < ApplicationController
 
 	def newOwner
 		logger.debug "/*********************** IPN Notification ********/"
-		setOwner(params[:payment_status], params[:payer_email], params[:item_name])
+		setOwner(params[:payment_status], params[:payer_email], params[:item_name], params[:paypal_secret])
 		
 
 		render :nothing => true
 	end
 
-	def setOwner (status, owner, item)
-		if status == "Completed"
+	def setOwner (status, owner, item, secret)
+		if status == "Completed" && secret == APP_CONFIG[:paypal_secret]
 			logger.debug "/*********************** IPN Notification COMPLETED ********/"
 			@currUser = User.find_by(emailPayment: owner)
 		  	@currentItem = Item.find_by_description(item)
