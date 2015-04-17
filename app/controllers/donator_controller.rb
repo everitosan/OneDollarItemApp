1d48fcd0a051bd7efa87eaeb0bbd6f7d034a7289
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-04-17 17:26:40 -0500

    fix (Paypal) log data send

--- app/controllers/donator_controller.rb
+++ app/controllers/donator_controller.rb
@@ -31,7 +31,8 @@ class DonatorController < ApplicationController
 
     crypChain = paypal_encrypted(params[:item], @User.id)
 
-    logger.debug params[:id] 
+    logger.debug "APP id: " + @User.id
+    logger.debug "Facebook id: " + params[:id] 
     logger.debug crypChain 
 
     render json: { status: :ok, owner: @User.id, item: params[:item], cryp: crypChain}
